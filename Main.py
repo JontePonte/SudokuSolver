86db96e0a4d9cdede4b6c6a224494b2a388e8774
JontePonte <john_nordstrand@hotmail.com>
2019-12-23 15:10:47 +0100

    Started to create the base

--- Main.py
+++ Main.py
@@ -1,6 +1,10 @@
 
 """ A program to solve a Suoku square """
 
+from Sudoku import sudoku
+from Square import Square
+
+
 class SudokuSolver:
     """ Main class to call everything """
     def __init__(self):
@@ -8,4 +12,5 @@ class SudokuSolver:
 
     def load_sudoku(self):
         """ Load sudoku from file """
-        pass
+        self.sudoku = sudoku
+        # create SudokuSquare from the sudoku file
---
+++ Square.py
@@ -0,0 +1,11 @@
+
+
+class Square:
+    """ Class for single square in the sudoku """
+    def __init__(self):
+        self.x_cor = 0      # x-coordinate
+        self.y_cor = 0      # y-coordinate
+        self.box = 0        # the box number the square belongs to
+
+        self.number = 0                                 # 0 means not defined
+        self.possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]     # Array with all possible values for the square

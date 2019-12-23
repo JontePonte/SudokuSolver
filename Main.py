388ca2d29e9a6f3010a6c3931d065db08ebb7ae8
JontePonte <john_nordstrand@hotmail.com>
2019-12-23 15:02:07 +0100

    Added the first files

---
+++ Main.py
@@ -0,0 +1,11 @@
+
+""" A program to solve a Suoku square """
+
+class SudokuSolver:
+    """ Main class to call everything """
+    def __init__(self):
+        self.load_sudoku()
+
+    def load_sudoku(self):
+        """ Load sudoku from file """
+        pass
